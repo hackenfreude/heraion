6d2206e750361187718c50228ccc67f4c5e58e09
hackenfreude <hackenfreude@users.noreply.github.com>
2018-05-13 21:42:23 -0700

    wire in en/decoders for Schema

--- src/main/scala/com/hackenfreude/heraion/Schema.scala
+++ src/main/scala/com/hackenfreude/heraion/Schema.scala
@@ -16,4 +16,12 @@
 
 package com.hackenfreude.heraion
 
+import io.circe.{ Decoder, Encoder }
+import io.circe.generic.semiauto._
+
 case class Schema()
+
+object Schema {
+  implicit val schemaDecoder: Decoder[Schema] = deriveDecoder[Schema]
+  implicit val schemaEncoder: Encoder[Schema] = deriveEncoder[Schema]
+}
