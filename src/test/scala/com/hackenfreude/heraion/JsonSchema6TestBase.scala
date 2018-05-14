866ac7abb4f515d1fc7d47d46959236bbafaafa1
hackenfreude <hackenfreude@users.noreply.github.com>
2018-05-13 21:47:11 -0700

    make base test harness for executing json-schema tests

---
+++ src/test/scala/com/hackenfreude/heraion/JsonSchema6TestBase.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 Hackenfreude
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hackenfreude.heraion
+
+import io.circe.parser._
+import io.circe.syntax._
+import org.scalatest._
+
+import scala.io.Source
+import scala.util.Try
+
+abstract class JsonSchema6TestBase(testResourceFileName: String) extends FunSpec with BeforeAndAfterAll with Matchers with AppendedClues {
+
+  private lazy val testResourceRelativePath = s"json-schema-test-suite/tests/draft6/$testResourceFileName"
+
+  private lazy val testCases: List[JsonSchemaTestCase] = {
+
+    val fileContents = Try(Source.fromResource(testResourceRelativePath).mkString).getOrElse("")
+    if (fileContents.isEmpty)
+      fail(
+        s"""Test harness could not read the test resource at $testResourceRelativePath.
+           |This indicates a problem with the harness itself.
+           |Good luck.""".stripMargin
+      )
+
+    decode[List[JsonSchemaTestCase]](fileContents) match {
+      case Left(_) => fail(
+        s"""Test harness could not read parse the contents of $testResourceRelativePath into test cases.
+           |This indicates a problem with the harness itself.
+           |Good luck.""".stripMargin
+      )
+      case Right(parsedSuites) => parsedSuites
+    }
+  }
+
+  //this is a hacky way to force reading and fail early
+  override def beforeAll(): Unit = {
+    val _ = testCases
+  }
+
+  describe(s"$testResourceRelativePath") {
+    for (testCase <- testCases) {
+      describe(s"${testCase.description}") {
+        for (test <- testCase.tests) {
+          it(s"${test.description}") {
+            Validator(test.data, testCase.schema) should be(test.valid) withClue
+              s"""
+                 |test data: ${test.data}
+                 |schema: ${testCase.schema.asJson}
+                 |ignore this line number:""".stripMargin
+          }
+        }
+      }
+    }
+  }
+}
